ef3768a4d620314dd56a7c99a0dbbccb4d4a030f
Zach Grafton <zachgrafton@gmail.com>
2020-08-17 00:25:07 -0700

    finished - added keyup event

--- dom.js
+++ dom.js
@@ -11,12 +11,8 @@ class Calculator {
         this.operator = undefined
     }
 
-    delete() {
-        this.currentOperand = this.currentOperand.toString().slice(0, -1)    
-
-    }
-
     appendNumber(number) {
+        // Refuses to allow a second decimal point
         if (number === '.' && this.currentOperand.includes('.')) return
         this.currentOperand = this.currentOperand.toString() + number.toString()
     }
@@ -35,7 +31,7 @@ class Calculator {
         let calculation
         const prev = parseFloat(this.previousOperand)
         const current = parseFloat(this.currentOperand)
-        if (isNan(prev || isNan(current))) return
+        if (isNaN(prev || isNaN(current))) return
         switch (this.operator) {
             case '+' :
                 calculation = prev + current
@@ -52,36 +48,56 @@ class Calculator {
             default:
                 return
         }
-
+        this.currentOperand= calculation
+        this.operator = undefined
+        this.previousOperand = ''
     }
 
     getDisplayNumber(number) {
         const stringNumber = number.toString()
-        const integerDigits = parseFloat(stringNumber.split('.')[0])
+        const integerDigits = parseFloat(stringNumber.split('.')[0]) // const?
         const decimalDigits = stringNumber.split('.')[1]
         let integerDisplay
         if (isNaN(integerDigits)) {
-            integerDigits = ''
+            integerDisplay = ''
         } else {
             integerDisplay = integerDigits.toLocaleString( 'en' , {
                 maximumFractionDigits: 0
             })
         }
-        if (decimalDigits =! null) {
-            return `${integerDisplay}`//.${decimalDigits}`
+        if (decimalDigits != null) {
+            return `${integerDisplay}.${decimalDigits}`
         } else {
+            console.log(integerDisplay)
             return integerDisplay
         }
     }
 
     updateDisplay() {
         this.currentOperandTextElement.innerText = this.getDisplayNumber(this.currentOperand)
-        if (this.operator!= null) {
+        if (this.operator != null) {
             this.previousOperandTextElement.innerText =
             `${this.getDisplayNumber(this.previousOperand)} ${this.operator}`
         } else {
             this.previousOperandTextElement.innerText = ''
         }
+    }
+
+    onKeyUp(event) {
+        let keyName = event.key
+        if (keyPressedNumbersAllowed.includes(keyName)) {
+            this.appendNumber(keyName)
+        }
+        if (keyPressedOperatorsAllowed.includes(keyName)) {
+            keyName == 'Enter' ? keyName = '=' : this.operator = keyName;
+            console.log(keyName);
+            // this.operator = keyName;
+            this.calculate();
+        }
+        if (keyName == 'Backspace') {
+            this.clear();
+        }
+        console.log(event)
     }
 }
 
@@ -89,10 +105,13 @@ const numberButtons = document.querySelectorAll('[data-number]')
 const operatorButtons = document.querySelectorAll('[data-operator]')
 const equalsButton = document.querySelector('[data-equals]')
 const deleteButton = document.querySelector('[data-delete]')
-const clearButton = document.querySelector('[data-clear')
 const allClearButton = document.querySelector('[data-clear-all]')
+const andyButton = document.querySelector('[data-andy]')
 const previousOperandTextElement = document.querySelector('[data-previous-operand]')
 const currentOperandTextElement = document.querySelector('[data-current-operand]')
+
+const keyPressedNumbersAllowed = ['0','1','2','3','4','5','6','7','8','9', '.'];
+const keyPressedOperatorsAllowed = ['/', '*', '-', '+', 'Enter'];
 
 const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
 
@@ -110,7 +129,7 @@ operatorButtons.forEach(button => {
     })
  })
 
- equalsButton.addEventListener('click', button => {
+equalsButton.addEventListener('click', button => {
      calculator.calculate()
      calculator.updateDisplay()
 })
@@ -120,7 +139,11 @@ allClearButton.addEventListener('click', button => {
     calculator.updateDisplay()
 })
 
-// deleteButton.addEventListener('click', button => {
-//     calculator.delete()
-//     calculator.updateDisplay()
-// })
+andyButton.addEventListener('click', button => {
+    location.href = "https://www.youtube.com/channel/UCZ9qFEC82qM6Pk-54Q4TVWA";
+})
+
+document.addEventListener('keyup', (event => {
+    calculator.onKeyUp(event)
+    calculator.updateDisplay()
+}))
